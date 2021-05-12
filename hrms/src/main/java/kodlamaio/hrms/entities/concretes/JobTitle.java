01c326c72b1b351fed7c58753836c49f5ec24239
gzmymn <77552353+gzmymn@users.noreply.github.com>
2021-05-12 15:41:33 +0300

    day6

--- hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobTitle.java
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobTitle.java
@@ -1,6 +1,7 @@
 package kodlamaio.hrms.entities.concretes;
 
 import javax.persistence.Column;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
